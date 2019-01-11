f998b34125801a6eb2a2a3b086789e235b29b3d6
“xsalina” <419593547@qq.comgit config --global user.email 419593547@qq.com>
2019-01-11 15:14:24 +0800

    @connect装饰器

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import {connect} from "react-redux";
-import {addstate,reducestate,removeAsync} from './redux/index.js'
+import {addstate,reducestate,removeAsync} from './redux/index.js';
+// //把state 映射到props中去   意思是吧redux的reducers返回的 新的状态 映射到react中的props中去
+// //见名知义
+// const mapStateToProps = state =>{
+//           return {xyz:state}
+// }
+// // 把各种dispatch也变成props 让你能够通过props使用
+// const mapDispatchToProps = {addstate,reducestate,removeAsync}
+//
+//
+// //连接 mapStateToProps，mapDispatchToProps
+// App = connect(mapStateToProps,mapDispatchToProps)(App)
+
+
+@connect(
+    state=>({xyz:state}),
+    {addstate,reducestate,removeAsync}
+)
+
 class App extends Component {
 
   render() {
@@ -33,7 +51,7 @@ class App extends Component {
         <br/>
         <input
             type="button"
-            defaultValue="异步操作减少数量"
+            defaultValue="异步操作减少数量state"
             onClick={()=>{
               this.props.removeAsync()
             }}
@@ -46,17 +64,17 @@ class App extends Component {
 
 
 
-//把state 映射到props中去   意思是吧redux的reducers返回的 新的状态 映射到react中的props中去
-//见名知义
-const mapStateToProps = state =>{
-          return {xyz:state}
-}
-// 把各种dispatch也变成props 让你能够通过props使用
-const mapDispatchToProps = {addstate,reducestate,removeAsync}
-
-
-//连接 mapStateToProps，mapDispatchToProps
-App = connect(mapStateToProps,mapDispatchToProps)(App)
+// //把state 映射到props中去   意思是吧redux的reducers返回的 新的状态 映射到react中的props中去
+// //见名知义
+// const mapStateToProps = state =>{
+//           return {xyz:state}
+// }
+// // 把各种dispatch也变成props 让你能够通过props使用
+// const mapDispatchToProps = {addstate,reducestate,removeAsync}
+//
+//
+// //连接 mapStateToProps，mapDispatchToProps
+// App = connect(mapStateToProps,mapDispatchToProps)(App)
 
 export default App;
 
